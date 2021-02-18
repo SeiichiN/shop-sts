2f4cbd39ab49c62c26604525b8e69705d82f9836
SeiichiN <billie175@gmail.com>
2021-02-18 20:55:40 +0900

    item listAll

--- src/main/java/jp/co/sss/shop/entity/Item.java
+++ src/main/java/jp/co/sss/shop/entity/Item.java
@@ -1,11 +1,15 @@
 package jp.co.sss.shop.entity;
 
 import javax.persistence.Column;
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
 
+@Entity
+@Table(name = "items")
 public class Item {
 	@Id
 	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_items_gen")
